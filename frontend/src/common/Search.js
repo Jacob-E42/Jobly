a26a009db6d28947f0cd705472afae28af2eecfd
Jacob Eiferman <jacobeiferman@gmail.com>
2023-03-28 11:50:50 -0400

    created generic Search Component

--- frontend/src/common/Search.js
+++ frontend/src/common/Search.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Form, Input, Button } from "reactstrap";
+import useForm from "../hooks/useForm";
 
-const Search = () => {};
+const Search = ({ allowedTerms }) => {
+	const [form, handleChange] = useForm({ searchTerm: "" });
+	const handleSubmit = useCallback(e => {
+		e.preventDefault();
+	});
+	return (
+		<Form onSubmit={handleSubmit}>
+			<Input
+				placeholder="Enter a Search Term..."
+				id="searchTerm"
+				name="searchTerm"
+				type="text"
+				value={form.searchTerm}
+				onChange={handleChange}
+			/>
+			<Button type="submit">Submit</Button>
+		</Form>
+	);
+};
 
 export default Search;

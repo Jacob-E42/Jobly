172d9ffc8d3d0561cf5207d179fe0f86d0c16041
Jacob Eiferman <jacobeiferman@gmail.com>
2023-03-30 14:20:05 -0400

    made Loading Spinner

---
+++ frontend/src/common/LoadingSpinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+/** Loading message used by components that fetch API data. */
+
+function LoadingSpinner() {
+	return <div className="LoadingSpinner">Loading ...</div>;
+}
+
+export default LoadingSpinner;

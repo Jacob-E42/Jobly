796b6e2e1726eb933cfecfa24730a02b3d759944
Jacob Eiferman <jacobeiferman@gmail.com>
2023-04-28 14:43:22 -0400

    fleshed out LoginForm

--- frontend/src/auth/LoginForm.js
+++ frontend/src/auth/LoginForm.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 const LoginForm = () => {
-	return <div>LoginForm</div>;
+	const [form, setForm] = useState({
+		username: "",
+		password: ""
+	});
+
+	const handleChange = event => {
+		setForm({
+			...form,
+			[event.target.name]: event.target.value
+		});
+	};
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		// handle form submission logic here
+	};
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<FormGroup>
+				<Label for="username">Username</Label>
+				<Input
+					type="text"
+					name="username"
+					id="username"
+					value={form.username}
+					onChange={handleChange}
+					placeholder="Enter your username"
+				/>
+			</FormGroup>
+			<FormGroup>
+				<Label for="password">Password</Label>
+				<Input
+					type="password"
+					name="password"
+					id="password"
+					value={form.password}
+					onChange={handleChange}
+					placeholder="Enter your password"
+				/>
+			</FormGroup>
+			<Button
+				color="primary"
+				type="submit">
+				Submit
+			</Button>
+		</Form>
+	);
 };
 
 export default LoginForm;

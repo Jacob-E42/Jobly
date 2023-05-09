3557f60c186c0a741a11b46c5f524a162686ca6d
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-09 13:52:24 -0400

    created useEffect to update CurrentUser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import UserContext from "./UserContext";
 import JoblyApi from "./api/api";
 import Router from "./routes/Router";
+import jwt from "jsonwebtoken";
 
 function App() {
-	const [tokin, setTokin] = useState(null);
+	const [token, setToken] = useState(null);
 	const [currentUser, setCurrentUser] = useState(null);
 
+	useEffect(
+		function loadUserInfo() {
+			console.debug("App useEffect loadUserInfo", "token=", token);
+
+			async function getCurrentUser() {
+				if (token) {
+					try {
+						let { username } = jwt.decode(token);
+						// put the token on the Api class so it can use it to call the API.
+						JoblyApi.token = token;
+						let currentUser = await JoblyApi.getCurrentUser(username);
+						setCurrentUser(currentUser);
+					} catch (err) {
+						console.error("App loadUserInfo: problem loading", err);
+						setCurrentUser(null);
+					}
+				}
+			}
+
+			getCurrentUser();
+		},
+		[token]
+	);
+
 	const login = (username, password) => {
-		const loginTokin = JoblyApi.login(username, password);
+		const logintoken = JoblyApi.login(username, password);
 
-		if (loginTokin) {
-			setTokin(tokin => loginTokin);
+		if (logintoken) {
+			setToken(token => logintoken);
 		}
-
-		setCurrentUser({ username, password });
 	};
 
 	const logout = () => {
-		setTokin(null);
+		setToken(null);
 		setCurrentUser(null);
 	};
 

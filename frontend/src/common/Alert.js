10645166cb0d27b85d0311de1a873668387c3437
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-15 12:30:44 -0400

    added comments to Alert.js

--- frontend/src/common/Alert.js
+++ frontend/src/common/Alert.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import { Alert } from "reactstrap";
 
 function AlertMessage({ msg, color = "primary" }) {
+	// State to control the visibility of the alert message
 	const [visible, setVisible] = useState(true);
 
+	// Function to dismiss the alert message
 	const onDismiss = () => setVisible(false);
 
 	return (
 		<Alert
+			// Set the color of the alert message
 			color={color}
+			// Specify if the alert is visible or hidden
 			isOpen={visible}
+			// Function to toggle the visibility of the alert
 			toggle={onDismiss}>
 			{msg}
 		</Alert>

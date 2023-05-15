507e381bd76f6e9d82765a07167ba4c31b677688
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-15 13:54:46 -0400

    added commments

--- frontend/src/common/Search.js
+++ frontend/src/common/Search.js
@@ -2,20 +2,26 @@ import React, { useCallback, useState } from "react";
 import { Form, Input, Button } from "reactstrap";
 
 const Search = ({ searchFor }) => {
+	// State to store the search term
 	const [searchTerm, setSearchTerm] = useState(null);
 
+	// Handle input change
 	const handleChange = useCallback(
 		e => {
 			e.preventDefault();
+			// Update the search term state with the new input value
 			setSearchTerm(e.target.value);
 		},
 		[searchTerm]
 	);
 
+	// Handle form submission
 	const handleSubmit = useCallback(
 		e => {
 			e.preventDefault();
+			// Perform a search using the search term (trimmed) or undefined if empty
 			searchFor(searchTerm.trim() || undefined);
+			// Update the search term state with the trimmed value
 			setSearchTerm(searchTerm.trim());
 		},
 		[searchFor, setSearchTerm, searchTerm]

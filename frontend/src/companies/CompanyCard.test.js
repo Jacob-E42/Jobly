8ad0ed06228bd94d2a2435f279b2c42a25dfa8de
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-28 09:31:49 -0400

    CompanyCard tests

--- frontend/src/companies/CompanyCard.test.js
+++ frontend/src/companies/CompanyCard.test.js
@@ -1,12 +1,46 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import CompanyCard from "./CompanyCard";
 
 test("CompanyCard renders without crashing", () => {
-	render(<CompanyCard />);
+	render(
+		<MemoryRouter>
+			<CompanyCard />
+		</MemoryRouter>
+	);
 });
 
 test("CompanyCard matches snapshot", () => {
-	const { asFragment } = render(<CompanyCard />);
+	const { asFragment } = render(
+		<MemoryRouter>
+			<CompanyCard />
+		</MemoryRouter>
+	);
+	expect(asFragment()).toMatchSnapshot();
+});
+
+test("CompanyCard matches snapshot with name and description", () => {
+	const { asFragment } = render(
+		<MemoryRouter>
+			<CompanyCard
+				name="Test Co"
+				desc="We provide tests"
+			/>
+		</MemoryRouter>
+	);
+	expect(asFragment()).toMatchSnapshot();
+});
+
+test("CompanyCard matches snapshot w/ name, desc and logo", () => {
+	const { asFragment } = render(
+		<MemoryRouter>
+			<CompanyCard
+				name="Test Co"
+				desc="We provide tests"
+				logo={"https://cdn.logo.com/hotlink-ok/logo-social.png"}
+			/>
+		</MemoryRouter>
+	);
 	expect(asFragment()).toMatchSnapshot();
 });

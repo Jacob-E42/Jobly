92cabb478aeb1763acf15e8c6fb8223132576056
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-28 09:31:59 -0400

    CompanyPage tests

--- frontend/src/companies/CompanyPage.test.js
+++ frontend/src/companies/CompanyPage.test.js
@@ -1,12 +1,31 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "../testMock";
 import CompanyPage from "./CompanyPage";
 
 test("CompanyPage renders without crashing", () => {
-	render(<CompanyPage />);
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				<CompanyPage />
+			</UserProvider>
+		</MemoryRouter>
+	);
 });
 
 test("CompanyPage matches snapshot", () => {
-	const { asFragment } = render(<CompanyPage />);
+	const { asFragment } = render(
+		<MemoryRouter initialEntries={["/company/ibm"]}>
+			<UserProvider>
+				<Routes>
+					<Route
+						path="/company/:handle"
+						element={<CompanyPage />}
+					/>
+				</Routes>
+			</UserProvider>
+		</MemoryRouter>
+	);
 	expect(asFragment()).toMatchSnapshot();
 });

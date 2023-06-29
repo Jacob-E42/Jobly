51b4769f576bf5948c0f692754546a55cba6a528
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-29 13:55:56 -0400

    conditionally displays login or logout options

--- frontend/src/routes/Nav.js
+++ frontend/src/routes/Nav.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import UserContext from "../UserContext";
 import "./Nav.css";
 
 const Nav = () => {
+	const { currentUser } = useContext(UserContext);
 	return (
 		<nav className="Nav">
 			<NavLink
@@ -12,9 +14,19 @@ const Nav = () => {
 			</NavLink>
 			<NavLink to="/companies">Companies</NavLink>
 			<NavLink to="/jobs">Jobs</NavLink>
-			<NavLink to="/login">Login</NavLink>
-			<NavLink to="/signup">Sign Up</NavLink>
-			<NavLink>Log Out</NavLink>
+			{!currentUser && (
+				<>
+					<NavLink to="/login">Login</NavLink>
+					<NavLink to="/signup">Sign Up</NavLink>
+				</>
+			)}
+
+			{currentUser && (
+				<>
+					<span>{currentUser.username}</span>
+					<NavLink>Log Out</NavLink>
+				</>
+			)}
 		</nav>
 	);
 };

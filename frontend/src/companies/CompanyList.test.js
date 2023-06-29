cfad6a65d0086abbe73bd5ba42e7fb536cc53b96
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-29 13:20:30 -0400

    JobCard tests

--- frontend/src/companies/CompanyList.test.js
+++ frontend/src/companies/CompanyList.test.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import CompanyList from "./CompanyList";
 
 test("CompanyList renders without crashing", () => {
-	render(<CompanyList />);
+	render(
+		<MemoryRouter>
+			<CompanyList />
+		</MemoryRouter>
+	);
 });
 
 test("CompanyList matches snapshot", () => {
-	const { asFragment } = render(<CompanyList />);
+	const { asFragment } = render(
+		<MemoryRouter>
+			<CompanyList />
+		</MemoryRouter>
+	);
 	expect(asFragment()).toMatchSnapshot();
 });
--- frontend/src/jobs/JobCard.test.js
+++ frontend/src/jobs/JobCard.test.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import JobCard from "./JobCard";
 
 test("JobCard renders without crashing", () => {
-	render(<JobCard />);
+	render(
+		<MemoryRouter>
+			<JobCard job={{ salary: "20000", equity: ".1", title: "snowman" }} />
+		</MemoryRouter>
+	);
 });
 
 test("JobCard matches snapshot", () => {
-	const { asFragment } = render(<JobCard />);
+	const { asFragment } = render(
+		<MemoryRouter>
+			<JobCard job={{ salary: "20000", equity: ".1", title: "snowman" }} />
+		</MemoryRouter>
+	);
 	expect(asFragment()).toMatchSnapshot();
 });

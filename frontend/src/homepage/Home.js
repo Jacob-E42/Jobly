106e9f0fd91e5e78467a6d87446246a20648c84a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-30 10:47:02 -0400

    homepage message

--- frontend/src/homepage/Home.js
+++ frontend/src/homepage/Home.js
@@ -1,5 +1,15 @@
-// import React from "react";
+import React, { useContext } from "react";
+import UserContext from "../UserContext";
 
-const Home = () => {};
+const Home = () => {
+	const { currentUser } = useContext(UserContext);
+
+	return (
+		<div>
+			{currentUser && <p>Welcome back {currentUser.firstName}!</p>}
+			{!currentUser && <p>Welcome to Jobly!</p>}
+		</div>
+	);
+};
 
 export default Home;

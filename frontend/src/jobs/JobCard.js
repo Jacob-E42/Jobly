6271cf3d193fd3369ba9f731af708af823f4c25b
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-03 11:06:04 -0400

    modified JobCard to use apply method

--- frontend/src/jobs/JobCard.js
+++ frontend/src/jobs/JobCard.js
@@ -1,27 +1,51 @@
-import React from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
+import React, { useContext, useState } from "react";
+import { Card, CardBody, CardTitle, CardText, Button } from "reactstrap";
 import addCommas from "./addCommas";
 import "./Jobs.css";
+import UserContext from "../UserContext";
 
 const JobCard = ({ job }) => {
+	const { currentUser, apply } = useContext(UserContext);
+	const [applied, setApplied] = useState(false);
+
+	const handleApply = async () => {
+		console.debug("handleApply");
+		try {
+			await apply(currentUser.username, job.id);
+			setApplied(true);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<Card
-			className="my-2 Card" // Custom CSS class for the Card component
+			className="my-2 Card"
 			color="dark"
-			outline // Render the Card component in an outlined style
-			style={{
-				width: "18rem"
-			}}>
+			outline
+			style={{ width: "18rem" }}>
 			{job && (
-				<CardBody>
-					<CardTitle tag="h5">{job.title}</CardTitle>{" "}
-					{/* Render the job title as a heading in the Card component */}
-					<CardText>
-						<span>Salary: {job.salary ? addCommas(job.salary) : "N/A"}</span>{" "}
-						{/* Render the job salary, or "N/A" if not available */}
-						<span>Equity: {job.equity ? job.equity : 0}</span>{" "}
-						{/* Render the job equity, or 0 if not available */}
+				<CardBody className="d-flex flex-column">
+					<CardTitle tag="h5">{job.title}</CardTitle>
+					<CardText className="mb-auto">
+						<span>Salary: {job.salary ? addCommas(job.salary) : "N/A"}</span>
+						<span>Equity: {job.equity ? job.equity : 0}</span>
 					</CardText>
+					{!applied ? (
+						<Button
+							color="danger"
+							onClick={handleApply}
+							className="mt-auto">
+							Apply
+						</Button>
+					) : (
+						<Button
+							color="secondary"
+							disabled
+							className="mt-auto">
+							Applied
+						</Button>
+					)}
 				</CardBody>
 			)}
 		</Card>

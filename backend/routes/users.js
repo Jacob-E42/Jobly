c8fb2e1a5a56bf3d61cf4ead587a51803f4c4052
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-03 11:05:41 -0400

    spelling mistake

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,7 +14,6 @@ const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
 
-
 /** POST / { user }  => { user, token }
  *
  * Adds a new user. This is not the registration endpoint --- instead, this is
@@ -28,21 +27,20 @@ const router = express.Router();
  **/
 
 router.post("/", ensureAdmin, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, userNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+	try {
+		const validator = jsonschema.validate(req.body, userNewSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map(e => e.stack);
+			throw new BadRequestError(errs);
+		}
 
-    const user = await User.register(req.body);
-    const token = createToken(user);
-    return res.status(201).json({ user, token });
-  } catch (err) {
-    return next(err);
-  }
+		const user = await User.register(req.body);
+		const token = createToken(user);
+		return res.status(201).json({ user, token });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 /** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
  *
@@ -52,32 +50,30 @@ router.post("/", ensureAdmin, async function (req, res, next) {
  **/
 
 router.get("/", ensureAdmin, async function (req, res, next) {
-  try {
-    const users = await User.findAll();
-    return res.json({ users });
-  } catch (err) {
-    return next(err);
-  }
+	try {
+		const users = await User.findAll();
+		return res.json({ users });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 /** GET /[username] => { user }
  *
- * Returns { username, firstName, lastName, isAdmin, jobs }
+ * Returns { username, firstName, lastName, isAdmin, applications }
  *   where jobs is { id, title, companyHandle, companyName, state }
  *
  * Authorization required: admin or same user-as-:username
  **/
 
 router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const user = await User.get(req.params.username);
-    return res.json({ user });
-  } catch (err) {
-    return next(err);
-  }
+	try {
+		const user = await User.get(req.params.username);
+		return res.json({ user });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 /** PATCH /[username] { user } => { user }
  *
@@ -90,20 +86,19 @@ router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, nex
  **/
 
 router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, userUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+	try {
+		const validator = jsonschema.validate(req.body, userUpdateSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map(e => e.stack);
+			throw new BadRequestError(errs);
+		}
 
-    const user = await User.update(req.params.username, req.body);
-    return res.json({ user });
-  } catch (err) {
-    return next(err);
-  }
+		const user = await User.update(req.params.username, req.body);
+		return res.json({ user });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 /** DELETE /[username]  =>  { deleted: username }
  *
@@ -111,14 +106,13 @@ router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, n
  **/
 
 router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    await User.remove(req.params.username);
-    return res.json({ deleted: req.params.username });
-  } catch (err) {
-    return next(err);
-  }
+	try {
+		await User.remove(req.params.username);
+		return res.json({ deleted: req.params.username });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 /** POST /[username]/jobs/[id]  { state } => { application }
  *
@@ -128,14 +122,13 @@ router.delete("/:username", ensureCorrectUserOrAdmin, async function (req, res, 
  * */
 
 router.post("/:username/jobs/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
-  try {
-    const jobId = +req.params.id;
-    await User.applyToJob(req.params.username, jobId);
-    return res.json({ applied: jobId });
-  } catch (err) {
-    return next(err);
-  }
+	try {
+		const jobId = +req.params.id;
+		await User.applyToJob(req.params.username, jobId);
+		return res.json({ applied: jobId });
+	} catch (err) {
+		return next(err);
+	}
 });
-
 
 module.exports = router;

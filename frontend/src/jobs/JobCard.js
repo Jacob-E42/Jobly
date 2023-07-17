ef57545e785ec287319f5a2c17b212a2d7d15a55
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-17 13:04:09 -0400

    apply() Alert

--- frontend/src/jobs/JobCard.js
+++ frontend/src/jobs/JobCard.js
@@ -4,9 +4,11 @@ import addCommas from "./addCommas";
 import UserContext from "../context_providers/UserContext";
 import ApplicationsContext from "../context_providers/ApplicationsContext";
 import "./Jobs.css";
+import AlertContext from "../context_providers/AlertContext";
 
 const JobCard = ({ job }) => {
 	if (!job) throw new Error("You must provide a job!");
+	const { setMsg, setColor } = useContext(AlertContext);
 	const { currentUser } = useContext(UserContext);
 	const { apply, applications } = useContext(ApplicationsContext);
 	const [applied, setApplied] = useState(applications.includes(job.id) ? true : false);
@@ -16,6 +18,8 @@ const JobCard = ({ job }) => {
 		try {
 			// if (currentUser.applications.includes(job.id))
 			await apply(currentUser.username, job.id);
+			setMsg("You applied!");
+			setColor("success");
 			setApplied(true);
 		} catch (error) {
 			console.error(error);

9e9a7bd6482d22d3b1288ff165b233316ab3018b
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-21 10:10:12 -0400

    new tests

--- frontend/src/common/Alert.test.js
+++ frontend/src/common/Alert.test.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Alert from "./Alert";
+import AlertExample from "./Alert";
+import { AlertProvider } from "../mock";
 
 test("Alert renders without crashing", () => {
-	render(<Alert />);
+	render(
+		<AlertProvider>
+			<AlertExample />
+		</AlertProvider>
+	);
 });
 
 test("Alert matches snapshot for success", () => {
-	const { asFragment } = render(<Alert msg={"I did it!"} />);
+	const { asFragment } = render(
+		<AlertProvider>
+			<AlertExample msg={"I did it!"} />
+		</AlertProvider>
+	);
 	expect(asFragment()).toMatchSnapshot();
 });
 
 test("Alert matches snapshot for danger", () => {
 	const { asFragment } = render(
-		<Alert
-			color="danger"
-			msg="You did not enter the correct information"
-		/>
+		<AlertProvider>
+			<AlertExample
+				color="danger"
+				msg="You did not enter the correct information"
+			/>
+		</AlertProvider>
 	);
 	expect(asFragment()).toMatchSnapshot();
 });
